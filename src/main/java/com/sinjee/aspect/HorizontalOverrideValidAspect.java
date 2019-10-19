ee14fe13418b1ec97845b61c122f62ed86cd4e8e
kweitan <chen_guixin@163.com>
2019-10-19 12:34:06 +0800

    横向越权控制实现 切面实现

--- src/main/java/com/sinjee/aspect/HorizontalOverrideValidAspect.java
+++ src/main/java/com/sinjee/aspect/HorizontalOverrideValidAspect.java
@@ -7,17 +7,20 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
 
 /**
  * @author 小小极客
  * 时间 2019/10/18 23:33
  * @ClassName HorizontalOverrideValidAspect
  * 描述 横向越权 切面
+ * Aspect 使用例子
  **/
 @Slf4j
 @Aspect
@@ -33,13 +36,18 @@ public class HorizontalOverrideValidAspect {
     /**
      * 环绕通知 进行校验
      * @param proceedingJoinPoint
-     * @param horizontalOverrideValid
      * @return
      */
-    @Around(value = "validPointcut() && @annotation(horizontalOverrideValid)")
-    public Object doAround(ProceedingJoinPoint proceedingJoinPoint, HorizontalOverrideValid horizontalOverrideValid){
+    @Around("validPointcut()")
+    public Object doAround(ProceedingJoinPoint proceedingJoinPoint){
+
+        MethodSignature signature = (MethodSignature)proceedingJoinPoint.getSignature() ;
+        Method method = signature.getMethod() ;
+
+        //取得方法上的注解
+        HorizontalOverrideValid horizontalOverrideValid = method.getAnnotation(HorizontalOverrideValid.class) ;
         String returnUrl = "" ;
-        if (horizontalOverrideValid != null){
+        if (null != horizontalOverrideValid){
             returnUrl = horizontalOverrideValid.returnUrl() ;
         }
 
@@ -47,6 +55,8 @@ public class HorizontalOverrideValidAspect {
         String userId = "" ;
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
         String passKey = request.getParameter("passkey") ;
+
+        //后台随机生成一个keys[useId+编号+随机号]
         String passKeyString = userId + "keys" ;
         String passKeyCreate = HashUtil.signKey(passKeyString,"UTF-8") ;
 
